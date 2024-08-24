a464dad74ee90345b6668e5192cdafb24f3b4a86
Komal <komalraut3102@gmail.com>
2024-08-24 21:54:30 +0530

    fix: update the key

--- src/page-section/builder/renderer/Renderer.tsx
+++ src/page-section/builder/renderer/Renderer.tsx
@@ -2,12 +2,13 @@ import { IField } from "@/types/builder";
 import React from "react";
 import { FieldRenderer } from "./FieldRenderer";
 
+
 export const Renderer: React.FC<{ fields: IField[] }> = ({ fields }) => {
   return (
     <div className="p-4 w-1/3 h-auto shadow-lg">
       <div className="flex flex-col items-center">
-        {fields.map((field, index) => (
-          <FieldRenderer key={index} field={field} />
+        {fields.map((field) => (
+          <FieldRenderer key={`${field.type}-${field.content}`} field={field} />
         ))}
       </div>
     </div>
